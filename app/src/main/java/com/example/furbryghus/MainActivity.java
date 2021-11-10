688282c2a545964bfff890dccda17d109a87c81e
casperkristiansen <71253446+casperkristiansen@users.noreply.github.com>
2021-11-10 12:58:35 +0100

    Firebase Authentication

--- app/src/main/java/com/example/furbryghus/MainActivity.java
+++ app/src/main/java/com/example/furbryghus/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.furbryghus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,8 +23,12 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 42;
     private ActivityMainBinding binding;
     private FirebaseAuth mAuth;
     private final static String TAG = "MainActivity";
@@ -51,7 +58,19 @@ public class MainActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser == null){
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.AnonymousBuilder().build());
 
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .setLogo(R.drawable.logo_d4d63234)
+                    .setTheme(R.style.FirebaseUI)
+                    .build();
+
+            startActivityForResult(signInIntent, RC_SIGN_IN);
         }
         updateUI(currentUser);
     }
