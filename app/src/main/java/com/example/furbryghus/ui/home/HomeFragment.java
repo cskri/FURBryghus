19401b87f8d6ae0d015bb64650ce21776d8917c8
casperkristiansen <71253446+casperkristiansen@users.noreply.github.com>
2021-11-17 13:17:33 +0100

    Slider on Home Fragment and Initial Beer Fragment Layout

--- app/src/main/java/com/example/furbryghus/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/furbryghus/ui/home/HomeFragment.java
@@ -14,16 +14,25 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.furbryghus.R;
 import com.example.furbryghus.databinding.FragmentHomeBinding;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
+    SliderView sliderView;
+    int[] images = {R.drawable.slider_hero,
+            R.drawable.slider_bygning,
+            R.drawable.slider_processen,
+            R.drawable.slider_sommerkoncert,
+            R.drawable.slider_tapning };
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -35,6 +44,15 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        /* SliderView */
+        sliderView = root.findViewById(R.id.image_slider);
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+
         return root;
     }
 
