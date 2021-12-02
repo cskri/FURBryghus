5650f9df08bdbc52d113446367c168219bc0912c
casperkristiansen <71253446+casperkristiansen@users.noreply.github.com>
2021-12-02 13:09:30 +0100

    Sustainability Profile

--- app/src/main/java/com/example/furbryghus/ui/Sustainability/SustainabilityViewModel.java
+++ app/src/main/java/com/example/furbryghus/ui/Sustainability/SustainabilityViewModel.java
@@ -3,5 +3,4 @@ package com.example.furbryghus.ui.Sustainability;
 import androidx.lifecycle.ViewModel;
 
 public class SustainabilityViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
 }
